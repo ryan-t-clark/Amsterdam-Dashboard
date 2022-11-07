a21637d3789f0f8798384f8b80d7f2fac222d67e
Ryan Clark <ryanclark.home@gmail.com>
2022-11-07 13:30:36 -0500

    some additions

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../App.css';
 
 const Dashboard = () => {
+
+    const [data, setData] = useState(undefined);
+
+    useEffect( () => {
+
+    },[])
+
     return (
         <div className="container">
             <h2 className="center">Dashboard</h2>
+
+            <a href="https://api.data.amsterdam.nl/api/">https://api.data.amsterdam.nl/api/</a>
+            <p>This website provides access to all API's provided by the city of Amsterdam</p>
         </div>
 
     )
--- src/components/Initiatives.js
+++ src/components/Initiatives.js
@@ -4,7 +4,24 @@ import '../App.css';
 const Initiatives = () => {
     return (
         <div className="container">
-            <h2 className="center">City Initiatives Towards becoming a Smart City</h2>
+            <h2 className="center">City Initiatives</h2>
+
+            <h3>The Amsterdam Smart City Initiative (ASC)</h3>
+            <p>As the title describes, the ASC is the program that aims to fund and facilitate the
+                implementation of smart city projects. They have led over 80 projects to the market,
+                focusing on the use of data to improve the wellbeing of citizens.
+            </p>
+            
+            
+            <h3>My Neighbor App</h3>
+            <p>MjinBurr, which translates to the My Neighbor App, is an App in the Amsterdam that
+                connects communities. Its goal is to allow neighbors to work through issues together
+                without the need for involving local government or police. Its meant to save time
+                and money for everyone involved by settling differences without escalation.
+            </p>
+
+            <h3>...</h3>
+            <p>More will go here</p>
         </div>
 
     )
